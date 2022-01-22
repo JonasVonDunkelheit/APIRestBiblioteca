6e502a827c3d5f2f81d5dadeb0183431be94ddfb
Andrés Felipe Babativa Betancourt <andresfelipe_1611@outlook.com>
2022-01-22 01:07:11 -0500

    Creación de DTOs

---
+++ Entities/DataTransferObjects/EstanteDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class EstanteDto
+    {
+        public Guid Id { get; set; }        
+        public string Codigo { get; set; }        
+        public Guid IdLibro { get; set; }                
+        public Guid IdRevista { get; set; }
+    }
+}
---
+++ Entities/DataTransferObjects/LibroDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public class LibroDto
+    {
+        public Guid Id { get; set; }
+        public string ISBN { get; set; }        
+        public string Titulo { get; set; }        
+        public string Formato { get; set; }        
+        public int NumPag { get; set; }        
+        public string IdEjemplar { get; set; }
+    }
+}
